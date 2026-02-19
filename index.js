6323b45779ea35efe0c122042cb4f55cb1343515
JuanCa9898 <JuanCRestrepo98@hotmail.com>
2026-02-19 00:17:23 -0500

    feat: add objects, arrays and functions exercises

--- index.js
+++ index.js
@@ -1,24 +1,102 @@
-let firstname = "Juan";
-let lastname = "Restrepo";
-let age = 27;
-
-let birthYear = 1998;
-let currentYear = 2026;
-let calculatedAge = currentYear - birthYear;
-
-if (calculatedAge >= 18) {
-    console.log("You are an adult.");
-} else {
-    console.log("You are a minor.")
+function findLargestNumber(arr) {
+    let largest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
 }
 
-let number = 7;
+let numbers = [100, 2, 45, 67, 3];
+//console.log(findLargestNumber(numbers));
 
-if (number % 2 == 0) {
-    console.log("Even number");
-} else {
-    console.log("Odd number");
+function getFirst(arr) {
+    return arr[0];
 }
 
-console.log("My name is " + firstname + " " + lastname);
-console.log("I am " + calculatedAge + " years old");
+//console.log(getFirst([10, 20, 30]));
+
+function findSmallestNumber(arr) {
+    let smallest = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+let numbers1 = [5, 12, 3, 20, 8];
+//console.log(findSmallestNumber(numbers1));
+
+function sumArray(arr) {
+    let total = 0;
+    for (let i =0 ; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+let numbers2 = [1, 2, 3, 4, 5];
+//console.log(sumArray(numbers2));
+
+function countEvens(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            count++;
+        }
+    }
+    return count;
+}
+let numbers3 = [1, 2, 3, 4, 6, 7, 8];
+//console.log(countEvens(numbers3));
+
+function printReverse(arr) {
+for (let i = arr.length -1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+}
+let numbers4 = [1, 2, 3, 4, 5];
+//printReverse(numbers4);
+
+//Objects
+
+let person = {
+    firstName : "Juan",
+    lastName : "Restrepo",
+    age : 27,
+    profession : "Engineer"
+}
+
+person.age = 28;
+person.country = "Colombia";
+
+//console.log(person);
+
+function describePerson (person) {
+    return person.firstName + " is " + person.age + " years old.";
+}
+
+//console.log(describePerson(person));
+
+let students = [
+    { name: "Ana", score: 90 },
+    { name: "Luis", score: 75 },
+    { name: "Maria", score: 95 }
+];
+
+//for (let i = 0; i < students.length; i++) {
+//    console.log(students[i].name + " scored " + students[i].score);
+//}
+
+function findTopStudent(students) {
+    let topStudent = students[0];
+    for (let i = 0; i < students.length; i++) {
+        if (students[i].score > topStudent.score) {
+            topStudent = students[i];
+        }
+    }
+    return "The top student is " +topStudent.name + " with a score of " + topStudent.score;
+}
+
+console.log(findTopStudent(students));
